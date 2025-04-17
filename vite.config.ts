619bda6703cfe98c3f199b29d5a967a526ef8e65
Shanti <marinkin.03@mail.ru>
2025-04-17 21:53:18 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pin-code/', // Укажите имя вашего репозитория
+  base: '/docs/', // Укажите имя вашего репозитория
   plugins: [react()],
 });
