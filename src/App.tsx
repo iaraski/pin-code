e46ef5334f75d43f63967d15b505dc35950c2355
Shanti <marinkin.03@mail.ru>
2025-06-15 21:03:53 +0300

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home.tsx";
 import Company from "./Pages/Company.tsx";
 import Blog from "./Pages/Blog.tsx";
@@ -6,33 +6,31 @@ import Products from "./Pages/Products.tsx";
 import Services from "./Pages/Services.tsx";
 import Portfolio from "./Pages/Portfolio.tsx";
 import Footer from "./Components/Footer.tsx";
-import "./styles/main.css"
+import "./styles/main.css";
 import Header from "./Components/Header.tsx";
-import"./styles/fonts.css"
+import "./styles/fonts.css";
 import Triager from "./Pages/Triager.tsx";
 import Janna from "./Pages/Janna.tsx";
 import Kastel from "./Pages/Kastel.tsx";
 import Consultation from "./Pages/Сonsultation.tsx";
 
-
 export default function App() {
-
-    return(
-        <Router>
-            <Header/>
+    return (
+        <Router basename="/pin-code">
+            <Header />
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/portfolio" element={<Portfolio/>} />
-                <Route path="/services" element={<Services/>} />
-                <Route path="/products" element={<Products/>} />
-                <Route path="/blog" element={<Blog/>} />
-                <Route path="/company" element={<Company/>} />
-                <Route path="/triager" element={<Triager/>}/>
-                <Route path ="/janna" element={<Janna/>}/>
-                <Route path="/kastel" element={<Kastel/>}/>
-                <Route path="/services/consultation/:serviceType" element={<Consultation/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/company" element={<Company />} />
+                <Route path="/triager" element={<Triager />} />
+                <Route path="/janna" element={<Janna />} />
+                <Route path="/kastel" element={<Kastel />} />
+                <Route path="/services/consultation/:serviceType" element={<Consultation />} />
             </Routes>
-            <Footer/>
+            <Footer />
         </Router>
-    )
+    );
 }
