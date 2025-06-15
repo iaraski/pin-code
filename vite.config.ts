ac3981d4f6cde60e5c5e81b1e0d1c215834ca491
Shanti <marinkin.03@mail.ru>
2025-06-15 20:57:05 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'; // Импорт плагина
 
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Важно для GitHub Pages
+  plugins: [react(), tsconfigPaths()], // Используем плагин
+  build: {
+    outDir: './docs', // Меняем dist на docs
+  },
 });
